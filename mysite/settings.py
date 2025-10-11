fc33ae9e95dacf7d83c551ba8bf1801a505ade8c
Ayush Satvara <ayushsatvara@MacBookPro.lan>
2025-10-11 12:56:13 -0400

    Fix environment variables for production

--- mysite/settings.py
+++ mysite/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-change-me')
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
